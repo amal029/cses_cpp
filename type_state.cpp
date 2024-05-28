9a8941756b5fe5022da7a38c90ff945c066277bc
Avinash Malik <avinash.malik@auckland.ac.nz>
2024-05-28 20:08:08 +1200

    Added constexpr, it makes everything run at compile time!
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- type_state.cpp
+++ type_state.cpp
@@ -15,9 +15,23 @@ template <class State> class Thread {};
 // XXX: Make a variant of thread state
 using ThreadState = std::variant<Thread<S0>, Thread<S1>, Thread<S2>>;
 
-// XXX: Thread data
+// XXX: Thread data -- this is kept separately to the thread states.
 struct ThreadData {
   int a = 0;
+  // XXX: Normal first time ctor
+  constexpr ThreadData(int _a) : a(_a) {}
+  // XXX: Need to declare a copy constructor all the time!
+  constexpr ThreadData(const ThreadData &d) : a(d.a) {}
+  // XXX: Copy assignment
+  constexpr ThreadData &operator=(const ThreadData &d) {
+    a = d.a;
+    return *this;
+  }
+  // XXX: Move assignment
+  constexpr ThreadData &operator=(ThreadData &&d) {
+    std::swap(d.a, a);
+    return *this;
+  }
   friend std::ostream &operator<<(std::ostream &, const ThreadData &);
 };
 
@@ -31,26 +45,26 @@ template <> class Thread<S0> {
   S0 _c;
 
 public:
-  Thread(S0 &&c) : _c(c) {}
+  constexpr Thread(S0 &&c) : _c(c) {}
   friend std::ostream &operator<<(std::ostream &s, const Thread<S0> &);
-  ThreadState tick(ThreadData &d);
+  constexpr ThreadState tick(ThreadData &d);
 };
 template <> class Thread<S1> {
   S1 _c;
 
 public:
-  Thread(S1 &&c) : _c(c) {}
+  constexpr Thread(S1 &&c) : _c(c) {}
   friend std::ostream &operator<<(std::ostream &s, const Thread<S0> &);
-  ThreadState tick(ThreadData &d);
+  constexpr ThreadState tick(ThreadData &d);
 };
 
 template <> class Thread<S2> {
   S2 _c;
 
 public:
-  Thread(S2 &&c) : _c(c) {}
+  constexpr Thread(S2 &&c) : _c(c) {}
   friend std::ostream &operator<<(std::ostream &s, const Thread<S0> &);
-  ThreadState tick(ThreadData &d);
+  constexpr ThreadState tick(ThreadData &d);
 };
 
 std::ostream &operator<<(std::ostream &s, const Thread<S0> &sd) {
@@ -72,7 +86,7 @@ std::ostream &operator<<(std::ostream &s, const Thread<S2> &sd) {
 }
 
 // XXX: Return by move
-ThreadState Thread<S0>::tick(ThreadData &d) {
+constexpr ThreadState Thread<S0>::tick(ThreadData &d) {
   if (d.a == 0) {
     d.a++;
     return S1{S1{}};
@@ -83,38 +97,40 @@ ThreadState Thread<S0>::tick(ThreadData &d) {
 }
 
 // XXX: Return by move
-ThreadState Thread<S1>::tick(ThreadData &d) {
+constexpr ThreadState Thread<S1>::tick(ThreadData &d) {
   d.a++;
   return S0{S0{}};
 }
 
-ThreadState Thread<S2>::tick(ThreadData &d) {
+constexpr ThreadState Thread<S2>::tick(ThreadData &d) {
   d.a--;
   return S0{S0{}};
 }
 
 // XXX: The initial state. This is a return by move!
-ThreadState init() { return S0{S0{}}; }
+constexpr ThreadState init() { return S0{S0{}}; }
 
 // XXX: Visit and call the tick function for the thread
 template <class... Ts> struct overloaded : Ts... {
   using Ts::operator()...;
 };
-ThreadState visit(ThreadState &&ts, ThreadData &d) {
+constexpr ThreadState visit(ThreadState &&ts, ThreadData &d) {
   return std::visit(overloaded{[&d](auto &t) { return t.tick(d); }}, ts);
 }
 
-void print_visit(const ThreadState &ts, const ThreadData &d) {
+constexpr void print_visit(const ThreadState &ts, const ThreadData &d) {
   return std::visit(overloaded{[&d](auto &t) { std::cout << t << d << "\n"; }},
                     ts);
 }
 
+// TODO: Write an example with async and 5 threads with computation.
+
 int main(int argc, char *argv[]) {
-  ThreadData data{};
-  auto v = init();
+  ThreadData data{0};
+  ThreadState v = init();
   print_visit(v, data);
   for (int i = 0; i < 10; ++i) {
-    // XXX: This assignment happens by copy, but why??
+    // XXX: This assignment happens by copy, OK just 1 byte of data.
     v = visit(std::move(v), data);
     print_visit(v, data);
   }
