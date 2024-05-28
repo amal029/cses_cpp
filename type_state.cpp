ac3f878f3875b66bfd724d8164d1a4930b56d7fd
Avinash Malik <avinash.malik@auckland.ac.nz>
2024-05-28 17:02:12 +1200

    Updated Type State example in C++. Now just need to model multi-threading in C++ to test what code to generate.
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- type_state.cpp
+++ type_state.cpp
@@ -7,12 +7,24 @@ class State {};
 // XXX: These are my states
 class S0 : State {};
 class S1 : State {};
+class S2 : State {};
 
 // XXX: This is the thread with data
 template <class State> class Thread {};
 
 // XXX: Make a variant of thread state
-using ThreadState = std::variant<Thread<S0>, Thread<S1>>;
+using ThreadState = std::variant<Thread<S0>, Thread<S1>, Thread<S2>>;
+
+// XXX: Thread data
+struct ThreadData {
+  int a = 0;
+  friend std::ostream &operator<<(std::ostream &, const ThreadData &);
+};
+
+std::ostream &operator<<(std::ostream &s, const ThreadData &d) {
+  s << d.a << " ";
+  return s;
+}
 
 // XXX: Specialise the Thread
 template <> class Thread<S0> {
@@ -20,62 +32,91 @@ template <> class Thread<S0> {
 
 public:
   Thread(S0 &&c) : _c(c) {}
-  constexpr Thread<S0> &operator=(const Thread<S0> &s) noexcept {
-    std::cout << "Copy assignment S0\n";
-    _c = s._c;
-    return *this;
-  };
-  // constexpr Thread<S0> &operator=(Thread<S0> &&s) noexcept {
-  //   std::swap(_c, s._c);
-  //   return *this;
-  // };
-  ThreadState tick();
+  friend std::ostream &operator<<(std::ostream &s, const Thread<S0> &);
+  ThreadState tick(ThreadData &d);
 };
 template <> class Thread<S1> {
   S1 _c;
 
 public:
   Thread(S1 &&c) : _c(c) {}
-  constexpr Thread<S1> &operator=(const Thread<S1> &s) noexcept {
-    std::cout << "Copy assignment S1\n";
-    _c = s._c;
-    return *this;
-  };
-  // constexpr Thread<S1> &operator=(Thread<S1> &&s) noexcept {
-  //   std::swap(_c, s._c);
-  //   return *this;
-  // };
-  ThreadState tick();
+  friend std::ostream &operator<<(std::ostream &s, const Thread<S0> &);
+  ThreadState tick(ThreadData &d);
 };
 
-// XXX: Return by copy
-ThreadState Thread<S0>::tick() {
-  std::cout << "S0 tick\n";
-  return S1{S1{}};
+template <> class Thread<S2> {
+  S2 _c;
+
+public:
+  Thread(S2 &&c) : _c(c) {}
+  friend std::ostream &operator<<(std::ostream &s, const Thread<S0> &);
+  ThreadState tick(ThreadData &d);
+};
+
+std::ostream &operator<<(std::ostream &s, const Thread<S0> &sd) {
+  s << "State S0"
+    << " ";
+  return s;
 }
 
-// XXX: Return by copy
-ThreadState Thread<S1>::tick() {
-  std::cout << "S1 tick\n";
+std::ostream &operator<<(std::ostream &s, const Thread<S1> &sd) {
+  s << "State S1"
+    << " ";
+  return s;
+}
+
+std::ostream &operator<<(std::ostream &s, const Thread<S2> &sd) {
+  s << "State S2"
+    << " ";
+  return s;
+}
+
+// XXX: Return by move
+ThreadState Thread<S0>::tick(ThreadData &d) {
+  if (d.a == 0) {
+    d.a++;
+    return S1{S1{}};
+  } else {
+    d.a--;
+    return S2{S2{}};
+  }
+}
+
+// XXX: Return by move
+ThreadState Thread<S1>::tick(ThreadData &d) {
+  d.a++;
   return S0{S0{}};
 }
 
-// XXX: This is a return by move!
+ThreadState Thread<S2>::tick(ThreadData &d) {
+  d.a--;
+  return S0{S0{}};
+}
+
+// XXX: The initial state. This is a return by move!
 ThreadState init() { return S0{S0{}}; }
 
+// XXX: Visit and call the tick function for the thread
 template <class... Ts> struct overloaded : Ts... {
   using Ts::operator()...;
 };
+ThreadState visit(ThreadState &&ts, ThreadData &d) {
+  return std::visit(overloaded{[&d](auto &t) { return t.tick(d); }}, ts);
+}
 
-ThreadState visit(ThreadState &ts) {
-  return std::visit(overloaded{[](auto &t) { return t.tick(); }}, ts);
+void print_visit(const ThreadState &ts, const ThreadData &d) {
+  return std::visit(overloaded{[&d](auto &t) { std::cout << t << d << "\n"; }},
+                    ts);
 }
 
 int main(int argc, char *argv[]) {
+  ThreadData data{};
   auto v = init();
+  print_visit(v, data);
   for (int i = 0; i < 10; ++i) {
     // XXX: This assignment happens by copy, but why??
-    v = visit(v);
+    v = visit(std::move(v), data);
+    print_visit(v, data);
   }
   return 0;
 }
